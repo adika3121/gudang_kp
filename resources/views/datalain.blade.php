be799e62eca1abb96418e853dd10ea7c2bf83c7f
cmeilani <cindymeilani98@gmail.com>
2019-01-10 15:15:03 +0800

    yuk pulang

--- resources/views/datalain.blade.php
+++ resources/views/datalain.blade.php
@@ -8,110 +8,129 @@
                     <div class="container-fluid">
                         <div class="row">
                             <div style="width:50%; float:left; padding-right:10px;">
-                            {{-- <div class="col-lg-5"> --}}
+                                {{-- <div class="col-lg-5"> --}}
                                 {{-- Data Kategori --}}
-                                <h3 style="padding:10px;">Kategori</h3>
-                                <div class="table-responsive table--no-card m-b-30 table-data">
-                                    <table class="table table-borderless table-striped table-earning">
-                                        {{-- <thead>
-                                            <tr>
-                                                <th>Data Kategori</th>
-                                            </tr>
-                                        </thead> --}}
-                                        <tbody>
-                                             @foreach($tampilKategori as $tp_ktgr)
-                                            <tr>
-                                                <td>{{$tp_ktgr->nama_kategori}}</td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+                                <div style="padding-top:15px;">
+                                    <div class="top-campaign" style="padding-top:5px; padding-bottom:20px;">
+                                        <h3 style="padding:10px;">Kategori</h3>
+                                            <div class="table-responsive table--no-card m-b-30 table-data">
+                                                <table class="table table-borderless table-striped table-earning">
+                                                    {{-- <thead>
+                                                        <tr>
+                                                            <th>Data Kategori</th>
+                                                        </tr>
+                                                    </thead> --}}
+                                                        <tbody>
+                                                             @foreach($tampilKategori as $tp_ktgr)
+                                                            <tr>
+                                                                <td>{{$tp_ktgr->nama_kategori}}</td>
+                                                            </tr>
+                                                            @endforeach
+                                                        </tbody>
+                                                </table>
+                                            </div>
+                                            <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalKategori">+ Tambah Kategori
+                                            </button>
+                                    </div>
                                 </div>
-                                <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalKategori">+ Tambah Kategori
-                                </button>
-                            {{-- </div> --}}
+                                {{-- </div> --}}
                             </div>
                             {{-- Data Merk --}}
                             <div style="width:50%; float:right;">
-                            {{-- <div class="col-lg-5"> --}}
-                                <h3 style="padding:10px;">Merk</h3>
-                                <div class="table-responsive table--no-card m-b-30 table-data">
-                                    <table class="table table-borderless table-striped table-earning">
-                                        {{-- <thead>
-                                            <tr>
-                                                <th>Data Merk</th>
-                                            </tr>
-                                        </thead> --}}
-                                        <tbody>
-                                            @foreach($tampilMerk as $tp_merk)
-                                            <tr>
-                                                <td>{{$tp_merk->nama_merek}}</td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+                                {{-- <div class="col-lg-5"> --}}
+                                    <div style="padding-top:15px;">
+                                        <div class="top-campaign" style="padding-top:5px; padding-bottom:20px;">
+                                            <h3 style="padding:10px;">Merk</h3>
+                                                <div class="table-responsive table--no-card m-b-30 table-data">
+                                                    <table class="table table-borderless table-striped table-earning">
+                                                        {{-- <thead>
+                                                            <tr>
+                                                                <th>Data Merk</th>
+                                                            </tr>
+                                                        </thead> --}}
+                                                        <tbody>
+                                                            @foreach($tampilMerk as $tp_merk)
+                                                            <tr>
+                                                                <td>{{$tp_merk->nama_merek}}</td>
+                                                            </tr>
+                                                            @endforeach
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                        {{-- </div> --}}
+                                        <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalMerk">+ Tambah Merk
+                                        </button>
+                                    </div>
                                 </div>
-                            {{-- </div> --}}
-                                <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalMerk">+ Tambah Merk
-                                </button>
                             </div>
                             {{-- Vendor --}}
-                            <h3 style="padding-top:30px; padding-bottom:10px;">Vendor</h3>
                             <div style="width:100%;">
-                                <div class="table-responsive table--no-card m-b-30">
-                                    <table class="table table-borderless table-striped table-earning">
-                                        <thead>
-                                            <tr>
-                                                <th>Nama Vendor</th>
-                                                <th>Alamat Vendor</th>
-                                                <th>No HP</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach($tampilVendor as $tp_vendor)
-                                            <tr>
-                                                <td>{{$tp_vendor->nama_vendor}}</td>
-                                                <td>{{$tp_vendor->alamat}}</td>
-                                                <td>{{$tp_vendor->no_telp}}</td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+                                <div style="padding-top:10px;">
+                                    <div class="top-campaign" style="padding-top:5px; padding-bottom:20px;">
+                                        <h3 style="padding-top:30px; padding-bottom:10px;">Daftar Vendor</h3>
+                                            <div class="table-responsive table--no-card m-b-30 table-data">
+                                                <table class="table table-borderless table-striped table-earning">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Nama Vendor</th>
+                                                            <th>Alamat Vendor</th>
+                                                            <th>No HP</th>
+                                                        </tr>
+                                                    </thead>
+                                                        <tbody>
+                                                            @foreach($tampilVendor as $tp_vendor)
+                                                            <tr>
+                                                                <td>{{$tp_vendor->nama_vendor}}</td>
+                                                                <td>{{$tp_vendor->alamat}}</td>
+                                                                <td>{{$tp_vendor->no_telp}}</td>
+                                                            </tr>
+                                                            @endforeach
+                                                        </tbody>
+                                                </table>
+                                            </div>
+                                            <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalVendor">+ Tambah Vendor
+                                            </button>
+                                    </div>
                                 </div>
-                                <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalVendor">+ Tambah Vendor
-                                </button>
                             </div>
+                        
                             {{-- Outlet --}}
-                            <h3 style="padding-top:30px; padding-bottom:10px;">Outlet</h3>
                             <div style="width:100%;">
-                                <div class="table-responsive table--no-card m-b-30">
-                                    <table class="table table-borderless table-striped table-earning">
-                                        <thead>
-                                            <tr>
-                                                <th>Kode Outlet</th>
-                                                <th>Nama Outlet</th>
-                                                <th>Alamat Outlet</th>
-                                                <th>No HP</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach($tampilOutlet as $tp_outlet)
-                                            <tr>
-                                                <td>{{$tp_outlet->kode_outlet}}</td>
-                                                <td>{{$tp_outlet->nama_outlet}}</td>
-                                                <td>{{$tp_outlet->alamat}}</td>
-                                                <td>{{$tp_outlet->no_telp}}</td>
-                                            </tr>
-                                            @endforeach
-                                        </tbody>
-                                    </table>
+                                <div style="padding-top:10px; padding-bottom:20px;">
+                                    <div class="top-campaign" style="padding-top:5px; padding-bottom:20px;">
+                                        <h3 style="padding-top:30px; padding-bottom:10px;">Daftar Outlet</h3>
+                                            <div class="table-responsive table--no-card m-b-30 table-data">
+                                                <table class="table table-borderless table-striped table-earning">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Kode Outlet</th>
+                                                            <th>Nama Outlet</th>
+                                                            <th>Alamat Outlet</th>
+                                                            <th>No HP</th>
+                                                        </tr>
+                                                    </thead>
+                                                        <tbody>
+                                                            @foreach($tampilOutlet as $tp_outlet)
+                                                            <tr>
+                                                                <td>{{$tp_outlet->kode_outlet}}</td>
+                                                                <td>{{$tp_outlet->nama_outlet}}</td>
+                                                                <td>{{$tp_outlet->alamat}}</td>
+                                                                <td>{{$tp_outlet->no_telp}}</td>
+                                                            </tr>
+                                                            @endforeach
+                                                        </tbody>
+                                                </table>
+                                            </div>
+                                            <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalOutlet">+ Tambah Outlet
+                                            </button>
+                                    </div>
                                 </div>
-                                <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#ModalOutlet">+ Tambah Outlet
-                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+
                 {{-- Modal Kategori --}}
                 <div class="modal fade" id="ModalKategori" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-lg" role="document">
@@ -251,6 +270,5 @@
                         </div>
                     </div>
                 </div>
-            </div>
 
 @endsection
