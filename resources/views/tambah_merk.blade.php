b437a1bb4dce486b4b4cefc304ddc6cd24fd791e
cmeilani <cindymeilani98@gmail.com>
2019-01-10 10:25:50 +0800

    morning

--- resources/views/tambah_merk.blade.php
+++ resources/views/tambah_merk.blade.php
@@ -12,7 +12,7 @@
                                     <table class="table table-borderless table-striped table-earning">
                                         <thead>
                                             <tr>
-                                                <th>Nama Merk</th>
+                                                <th>Data Merk</th>
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -24,36 +24,41 @@
                                         </tbody>
                                     </table>
                                 </div>
+                                    <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#largeModal">+ Tambah Merk
+                                    </button>
                             </div>
-                           
-                        <div class="col-lg-3">
-                                <div class="card" style="center">
-                                    <div class="card-header">
-                                        <strong>Tambahkan Merk</strong>
-                                    </div>
-                                    <div class="card-body card-block">
-                                        <form action="{{action('TbMerekController@store')}}" method="post" class="">
-                                        {{ csrf_field() }}
-                                            <div class="form-group">
-                                                <label for="nf-email" class=" form-control-label">Nama Merk</label>
-                                                <input type="text" id="nf-email" name="nama_merek" placeholder="Masukkan nama merk.." class="form-control">
-                                            </div>
-                                            <button type="submit" class="btn btn-primary btn-sm">
-                                                <i class="fa fa-dot-circle-o"></i> Simpan
-                                            </button>
-                                            <button type="reset" class="btn btn-danger btn-sm">
-                                                <i class="fa fa-ban"></i> Batal
-                                    </div>
-                            
                         </div>
-                   
+                    </div>
+                </div>
+                <div class="modal fade" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="largeModalLabel">Tambahkan Merk Baru</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
+                    <div class="modal-body">
+                        <form action="{{action('TbMerekController@store')}}" method="post" class="">
+                            {{ csrf_field() }}
+                            <div class="form-group">
+                                <label for="nf-email" class=" form-control-label">Nama Merek</label>
+                                <input type="text" id="nf-email" name="nama_merek" placeholder="Masukkan nama merk.." class="form-control">
+                            </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-primary btn-sm">
+                                <i class="fa fa-dot-circle-o"></i> Simpan
+                            </button>
+                            <button type="reset" class="btn btn-danger btn-sm">
+                                <i class="fa fa-ban"></i> Batal
+                            </button>
+                        </div>          
+                        </form>    
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-    </div>
 
 @endsection
--- resources/views/tambah_outlet.blade.php
+++ resources/views/tambah_outlet.blade.php
@@ -7,7 +7,7 @@
                 <div class="section__content section__content--p30">
                     <div class="container-fluid">
                         <div class="row">
-                            <div class="col-lg-9">
+                            <div style="width:100%;">                                <h2 class="title-1 m-b-25">Data Outlet</h2>
                                 <div class="table-responsive table--no-card m-b-30">
                                     <table class="table table-borderless table-striped table-earning">
                                         <thead>
@@ -30,16 +30,24 @@
                                         </tbody>
                                     </table>
                                 </div>
+                                    <button type="button" class="au-btn au-btn-icon au-btn--green au-btn--small" data-toggle="modal" data-target="#largeModal">+ Tambah Outlet
+                                    </button>
                             </div>
-                           
-                        <div class="col-lg-3">
-                                <div class="card" style="center">
-                                    <div class="card-header">
-                                        <strong>Tambahkan Outlet</strong>
-                                    </div>
-                                    <div class="card-body card-block">
-                                        <form action="{{action('TbOutletController@store')}}" method="post" class="">
-                                        {{ csrf_field() }}
+                        </div>
+                    </div>
+                </div>
+                <div class="modal fade" id="largeModal" tabindex="-1" role="dialog" aria-labelledby="largeModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-lg" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="largeModalLabel">Tambahkan Outlet Baru</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                    <div class="modal-body">
+                        <form action="{{action('TbOutletController@store')}}" method="post" class="">
+                            {{ csrf_field() }}
                                             <div class="form-group">
                                                 <label for="nf-email" class=" form-control-label">Kode Outlet</label>
                                                 <input type="text" id="nf-email" name="kode_outlet" placeholder="Masukkan kode outlet.." class="form-control">
@@ -69,8 +77,5 @@
                    
                         </div>
                         </div>
-                    </div>
-                </div>
-    
 
 @endsection
